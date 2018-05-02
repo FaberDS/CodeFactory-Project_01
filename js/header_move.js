d94896851b71691133ffbab6447da7c0519e1f44
Denis Schuele <schueledenis@gmx.at>
2018-05-02 17:40:06 +0200

    header mov

--- js/header_move.js
+++ js/header_move.js
@@ -7,41 +7,40 @@ var topAni = document.getElementById("cLettercLetter");
 var right = document.getElementById("rLetter");
 var back = document.getElementById("letters");
 
-// media query event handler-----------------------------------------------------
-// const mq = window.matchMedia( "(min-width: 500px)" );
-// if (matchMedia) {
-//   const mq = window.matchMedia("(min-width: 600px)");
-//   mq.addListener(WidthChange);
-//   WidthChange(mq);
-// }
 
-//media query change-----------------------------------------
-// function WidthChange(mq) {
-//   if (mq.matches) {
-//     // window width is at least 600px---------------------------
-//     magic();
-   
-//   } else {
-//     //window width is less than 600px--------------------------
-//     magicSmall();
-//   }
 
-// }
+// media query change-----------------------------------------
+var width = window.innerWidth
+console.log(width);
+function WidthChange() {
+
+  if (width >= 800) {
+    // window width is at least 600px---------------------------
+    magic();
+    
+   console.log("if2");
+  } else if (width >= 600) {
+    //window width is less than 600px--------------------------
+    magicSmall();
+     console.log("else");
+  }
+
+}
 function magic(){
 	// left.style.transform= "translate(-170px,0)";
-	right.style.transform= "translate(485px,0)";
+	right.style.transform= "translate(465px,0)";
 	back.style.opacity= "1";
 	setTimeout(magicReturn, 4000);
 }
 
-// function magicSmall(){
-// 	x += 1;
-// 	console.log(x);
-// 	// left.style.transform= "translate(-170px,0)";
-// 	right.style.transform= "translate(300px,0)";
-// 	back.style.opacity= "1";
-// 	setTimeout(magicReturn, 4000);
-// }
+function magicSmall(){
+	x += 1;
+	console.log(x);
+	// left.style.transform= "translate(-170px,0)";
+	right.style.transform= "translate(280px,0)";
+	back.style.opacity= "1";
+	setTimeout(magicReturn, 4000);
+}
 
 function magicReturn(){
 	inVisible();
