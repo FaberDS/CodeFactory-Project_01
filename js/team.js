78148325a01e3f9b980e6ca24b26acc8bb9d475f
Anna <anna0706@gmx.at>
2018-05-03 00:44:48 +0200

    images

--- js/team.js
+++ js/team.js
@@ -9,10 +9,10 @@
     var print2 = document.getElementById("card2");
     var print3 = document.getElementById("card3"); 
 console.log(teamMember0);   
-	print0.innerHTML = "</br>" + "<b>" + teamMember0.name + "</b>" +"</br>" + "<i>" + teamMember0.position + "</i>" + "</br>" + "Location: " + teamMember0.region + "</br>" + teamMember0.mail;
-    print1.innerHTML = "</br>" + "<b>" + teamMember1.name + "</b>" + "</br>" + "<i>" + teamMember1.position + "</i>" + "</br>" + "Location: " + teamMember1.region + "</br>" + teamMember1.mail;
-    print2.innerHTML = "</br>" + "<b>" + teamMember2.name + "</b>" + "</br>" + "<i>" + teamMember2.position + "</i>" + "</br>" + "Location: " + teamMember2.region + "</br>" + teamMember2.mail;
-    print3.innerHTML = "</br>" + "<b>" + teamMember3.name + "</b>" + "</br>" + "<i>" + teamMember3.position + "</i>" + "</br>" + "Location: " + teamMember3.region + "</br>" + teamMember3.mail;
+	print0.innerHTML = "<b>" + teamMember0.name + "</b>" +"</br>" + "<i>" + teamMember0.position + "</i>" + "</br>" + "Location: " + teamMember0.region + "</br>" + teamMember0.mail;
+    print1.innerHTML = "<b>" + teamMember1.name + "</b>" + "</br>" + "<i>" + teamMember1.position + "</i>" + "</br>" + "Location: " + teamMember1.region + "</br>" + teamMember1.mail;
+    print2.innerHTML = "<b>" + teamMember2.name + "</b>" + "</br>" + "<i>" + teamMember2.position + "</i>" + "</br>" + "Location: " + teamMember2.region + "</br>" + teamMember2.mail;
+    print3.innerHTML = "<b>" + teamMember3.name + "</b>" + "</br>" + "<i>" + teamMember3.position + "</i>" + "</br>" + "Location: " + teamMember3.region + "</br>" + teamMember3.mail;
 
 
 
