b864cd5f02cc4ca57fb0ed0c5cdf7f22d6490257
Dominik Link <Vorbild94@gmail.com>
2018-05-03 11:55:12 +0200

    fml never gonna do it again

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -19,29 +19,37 @@ function dragStart(ev)
 
 function drop(ev)
 {
-  ev.target.append(document.getElementById(id))
+  //ev.target.append(document.getElementById(id))
 }
 
 function cheese2()
 {
+  var image = document.getElementById("changedat")
   sum = pizza + cheese
   document.getElementById("calculator").innerHTML = "The calories of a Pizza with cheese are" + sum
+  image.src = "./img/cheesepizza.jpg"
 }
 
 function ham2()
 {
+  var image = document.getElementById("changedat")
   sum = pizza + ham
   document.getElementById("calculator").innerHTML = "The calories of a Pizza with ham are" + sum
+  image.src = "./img/hampizza.png"
 }
 
 function salami2()
 {
+  var image = document.getElementById("changedat")
   sum = pizza + salami
   document.getElementById("calculator").innerHTML = "The calories of a Pizza with salami are" + sum
+  image.src = "./img/salamipizza.png"
 }
 
 function corn2()
 {
+  var image = document.getElementById("changedat")
   sum = pizza + corn
   document.getElementById("calculator").innerHTML = "The calories of a Pizza with corn are" + sum
+  image.src = "./img/cornpizza.png"
 }
